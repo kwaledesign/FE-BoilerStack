52c15cd216236a43ef888747925543a79c35004b
Sam Ashley <sashley@providigm.com>
2015-02-22 07:39:24 -0700

    init additional routes

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// create routes for the admin section
+
+// get an instance of the router
+var adminRouter = express.Router();
+
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res) {
+  res.send('I am the dashboard!');
+});
+
+// users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
+  res.send('I show all the users!');
+});
+
+// posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+  res.send('I show all the posts!');
+});
+
+// apply the routes to our application
+app.use('/admin', adminRouter);
+
+
+
 // start the server
 app.listen(1337);
 console.log('1337 is the magic port!');
