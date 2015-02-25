7264e38e8181c4682dd032975f53c6411549c4a9
Sam Ashley <sashley@providigm.com>
2015-02-24 19:38:44 -0700

    impliment authentication for all api/ routing'

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ var morgan     = require('morgan'); // used to see requests
 var mongoose   = require('mongoose'); // for working w/ our database
 var port       = process.env.PORT || 8080; // set the port for our app
 var User       = require('./app/models/user'); // require user model
+var jwt        = require('jsonwebtoken'); // auth via token lib
 
+// set jwt secret
+var superSecret = 'mysecretstringnoonewilleverguesseventhoughitsingithub';
 
 // DATABASE SETUP
 // connect to database hosted on mongolab
@@ -34,41 +37,104 @@ app.use(function(req, res, next) {
 app.use(morgan('dev'));
 
 // ROUTES FOR OUR API
-// =============================
+// ======================================
+
+// basic route for the home page
+app.get('/', function(req, res) {
+  res.send('Welcome to the home page!');
+});
 
 // get an instance of the express router
 var apiRouter = express.Router();
 
-// middleware to use for all requests
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRouter.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user) {
+
+    if (err) throw err;
+
+    // no user with that username was found
+    if (!user) {
+      res.json({ 
+        success: false, 
+        message: 'Authentication failed. User not found.' 
+      });
+    } else if (user) {
+
+      // check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({ 
+          success: false, 
+          message: 'Authentication failed. Wrong password.' 
+        });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+// route middleware to verify a token
 apiRouter.use(function(req, res, next) {
   // do logging
   console.log('Somebody just came to our app!');
 
-  // we'll add more to the middleware in Chapter 10
-  // this is where we will authenticate users
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 
-  next(); // make sure we go to the next routes and don't stop here
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });   
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next(); // make sure we go to the next routes and don't stop here
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an HTTP response of 403 (access forbidden) and an error message
+    return res.status(403).send({ 
+      success: false, 
+      message: 'No token provided.' 
+    });
+    
+  }
 });
 
-
-// basic route for the home page
-//app.get('/', function(req, res) {
-//  res.send('Welcome to the home page!');
-//});
-
-// test route to make sure everything is working
-
+// test route to make sure everything is working 
 // accessed at GET http://localhost:8080/api
 apiRouter.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });	
+  res.json({ message: 'hooray! welcome to our api!' }); 
 });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', apiRouter);
-
 
 // on routes that end in /users
 // ----------------------------------------------------
@@ -100,14 +166,12 @@ apiRouter.route('/users')
   // get all the users (accessed at GET http://localhost:8080/api/users)
   .get(function(req, res) {
     User.find(function(err, users) {
-      if (err) return res.send(err);
+      if (err) res.send(err);
 
       // return the users
       res.json(users);
     });
   });
-
-
 
 // on routes that end in /users/:user_id
 // ----------------------------------------------------
@@ -116,7 +180,7 @@ apiRouter.route('/users/:user_id')
   // get the user with that id
   .get(function(req, res) {
     User.findById(req.params.user_id, function(err, user) {
-      if (err) return res.send(err);
+      if (err) res.send(err);
 
       // return that user
       res.json(user);
@@ -127,7 +191,7 @@ apiRouter.route('/users/:user_id')
   .put(function(req, res) {
     User.findById(req.params.user_id, function(err, user) {
 
-      if (err) return res.send(err);
+      if (err) res.send(err);
 
       // set the new user information if it exists in the request
       if (req.body.name) user.name = req.body.name;
@@ -136,7 +200,7 @@ apiRouter.route('/users/:user_id')
 
       // save the user
       user.save(function(err) {
-        if (err) return res.send(err);
+        if (err) res.send(err);
 
         // return a message
         res.json({ message: 'User updated!' });
@@ -150,12 +214,19 @@ apiRouter.route('/users/:user_id')
     User.remove({
       _id: req.params.user_id
     }, function(err, user) {
-      if (err) return res.send(err);
+      if (err) res.send(err);
 
       res.json({ message: 'Successfully deleted' });
     });
   });
 
+// api endpoint to get user information
+apiRouter.get('/me', function(req, res) {
+  res.send(req.decoded);
+});
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', apiRouter);
 
 
 // START THE SERVER
